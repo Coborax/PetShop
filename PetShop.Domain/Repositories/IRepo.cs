665bdcdadc9ceca6d2e57f034f0fc82bef5e79bc
Mikkel Mouridsen <mikkelmouridsen@protonmail.com>
2021-08-31 12:31:57 +0200

    Added PetRepo

--- PetShop.Domain/Repositories/IRepo.cs
+++ PetShop.Domain/Repositories/IRepo.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
-using PetShop.Core.Models;
 
 namespace PetShop.Domain.Repositories
 {
     public interface IRepo<T>
     {
         List<T> GetAll();
+        T Find(int id);
+
     }
 }
